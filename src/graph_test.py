09fa52d2d07292ea025b8355cc74c472e81f28cc
David Goldstein <dgoldstein01@gmail.com>
2019-05-04 15:55:52 -0400

    added empty tests for graph

---
+++ src/graph_test.py
@@ -0,0 +1,13 @@
+import unittest
+import graph
+
+class TestGraphMethods(unittest.TestCase):
+
+	def test_addNeighbors(self):
+		pass
+
+	def test_getShortestPath(sel):
+		pass
+
+	def test_g(self):
+		self.assertIsNotNone(graph.Graph().g())
