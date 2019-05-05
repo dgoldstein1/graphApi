33ed55f5293f52983fe80ea298ef1067e41ac7ed
David Goldstein <dgoldstein01@gmail.com>
2019-05-04 22:38:30 -0400

    able to redirect to metrics

--- src/server.py
+++ src/server.py
@@ -1,22 +1,26 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, redirect
 from flask_restful import Resource, Api
 from json import dumps
 from flask_prometheus import monitor 
 
+# constants
+METRICS_PORT = 8001
+
+# flask setup
 app = Flask(__name__)
 api = Api(app)
-monitor(app, port=8000)
+monitor(app, port=METRICS_PORT)
 
 class ServeDocs(Resource):
 	"""Serves docs to browser"""
 	def get(self):
 		return send_file("../api/index.html")
 
-class serveMetrics(Resource):
+class ServeMetrics(Resource):
 	"""server prometheus metrics"""
 	def get(self):
 		# TODO: make request to monitored port
-		pass		
+		return redirect("http://127.0.0.1:{}".format(METRICS_PORT))
 
 class Neighbors(Resource):
 	"""adds neighbor nodes to graph. Returns {error: on error}"""
@@ -33,7 +37,7 @@ class ShortestPath(Resource):
     
 
 api.add_resource(ServeDocs, '/')
-api.add_resource(ServeDocs, '/metrics')
+api.add_resource(ServeMetrics, '/metrics')
 api.add_resource(Neighbors, '/neighbors')
 api.add_resource(ShortestPath, '/shortestPath')
 
