0a575ea9c02a42a5396ad7ed787b6abd1d04e663
dgoldstein1 <david.goldstein@deciphernow.com>
2019-09-13 14:02:39 -0400

    trying to exit on bad tests

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -2,7 +2,14 @@
 echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
 
 export GRAPH_SAVE_PATH="$(pwd)/out/test1.graph"
+
+
 coverage run -m pytest
+if [[ $? != 0 ]]; then
+    echo "Tests have failed"
+    exit 1
+fi
+
 
 if [[ "$CIRCLE_BRANCH" == "master" ]]; then
   export CC_TEST_REPORTER_ID=a16ce4a8d9e5d0398c6cbf335aafe3438b374e3cb0c1274a3eb3701cb8689c14
