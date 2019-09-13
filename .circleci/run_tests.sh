009c1cb3adf10d09289d0ab7b87c483bf476e7b4
David Goldstein <dgoldstein01@gmail.com>
2019-09-13 14:14:08 -0400

    Fix ci test (#20)
    * reorganized circle config
    
    * Trying docker approach
    
    * trying docker image caching
    
    * building each time
    
    * trying to exit on bad tests
    
    * added docker ignore
    
    * testing is working locally

---
+++ .circleci/run_tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
+
+export GRAPH_SAVE_PATH="$(pwd)/out/test1.graph"
+
+> config.cfg
+echo 'METRICS_PORT = 8001
+SHORTEST_PATH_TIMEOUT = 3000
+METRICS_HOST="http://127.0.0.1"
+HOST = "http://127.0.0.1"
+TESTING = "True"' >> config.cfg
+
+coverage run -m pytest
+if [[ $? != 0 ]]; then
+    echo "Tests have failed"
+    exit 1
+fi
+
+
+if [[ "$CIRCLE_BRANCH" == "master" ]]; then
+  export CC_TEST_REPORTER_ID=a16ce4a8d9e5d0398c6cbf335aafe3438b374e3cb0c1274a3eb3701cb8689c14
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+  chmod +x ./cc-test-reporter
+  coverage xml
+  ./cc-test-reporter format-coverage coverage.xml -t coverage.py
+  ./cc-test-reporter upload-coverage
+fi
